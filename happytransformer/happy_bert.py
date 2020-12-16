2960a3faf465419db1146b54c50d8dafbd99d8c2
Kiran Akadas <43413124+KiranAkadas@users.noreply.github.com>
2020-12-16 20:02:05 +0530

    Update happy_bert.py

--- happytransformer/happy_bert.py
+++ happytransformer/happy_bert.py
@@ -12,13 +12,24 @@ from transformers import (
     BertForQuestionAnswering,
     BertTokenizer
 )
-
+from transformers import DistilBertTokenizerFast
 import torch
 import pandas as pd
 from torch.utils.data import DataLoader
 from transformers import AdamW
 from happytransformer.happy_transformer import HappyTransformer
 
+import torch
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, encodings):
+        self.encodings = encodings
+
+    def __getitem__(self, idx):
+        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+
+    def __len__(self):
+        return len(self.encodings.input_ids)
 
 class HappyBERT(HappyTransformer):
     """
@@ -43,7 +54,7 @@ class HappyBERT(HappyTransformer):
         self.mlm = None  # Masked Language Model
         self.nsp = None  # Next Sentence Prediction
         self.qa = None   # Question Answering
-        self.tokenizer = BertTokenizer.from_pretrained(model)
+        self.tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
         self.masked_token = self.tokenizer.mask_token
         self.sep_token = self.tokenizer.sep_token
         self.cls_token = self.tokenizer.cls_token
@@ -57,10 +68,13 @@ class HappyBERT(HappyTransformer):
 
     def read_dataset(self,path):
         f=pd.read_excel(path, engine='openpyxl')
-        con1= f['CONTEXT'].tolist()
-        a1= f['a1'].tolist()
-        a2= f['a2'].tolist()
-        a3= f['a3'].tolist()
+        con1= f['context'].tolist()
+        a1= f['time-start'].tolist()
+        a2= f['time-end'].tolist()
+        a3= f['date-start'].tolist()
+        a4= f['date-end'].tolist()
+        a5= f['user-start'].tolist()
+        a6= f['user-end'].tolist()
         q=["What time is the appointment?","what calendar date is the appointment?","who is the appointment with?"]
         contexts = []
         questions = []
@@ -68,13 +82,13 @@ class HappyBERT(HappyTransformer):
         for i,c in enumerate(con1):
             contexts.append(c)
             questions.append(q[0])
-            answers.append(a1[i])
+            answers.append({"answer_start":a1[i],"answer_end":a2[i]})
             contexts.append(c)
             questions.append(q[1])
-            answers.append(a2[i])
+            answers.append({"answer_start":a3[i],"answer_end":a4[i]})
             contexts.append(c)
             questions.append(q[2])
-            answers.append(a3[i])
+            answers.append({"answer_start":a5[i],"answer_end":a6[i]})
 
         return contexts, questions, answers
 
@@ -86,15 +100,15 @@ class HappyBERT(HappyTransformer):
             end_positions.append(encodings.char_to_token(i, answers[i]['answer_end'] - 1))
             # if None, the answer passage has been truncated
             if start_positions[-1] is None:
-                start_positions[-1] = tokenizer.model_max_length
+                start_positions[-1] = self.tokenizer.model_max_length
             if end_positions[-1] is None:
-                end_positions[-1] = tokenizer.model_max_length
+                end_positions[-1] = self.tokenizer.model_max_length
         encodings.update({'start_positions': start_positions, 'end_positions': end_positions})
 
     def train(self):
         self.model=BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-        train_contexts,train_questions,train_answers = self.read_dataset("dataset.xlsx")
-        val_contexts,val_questions,val_answers = self.read_dataset("valdataset.xlsx")
+        train_contexts,train_questions,train_answers = self.read_dataset("dataset1.xlsx")
+        val_contexts,val_questions,val_answers = self.read_dataset("valdataset1.xlsx")
         train_encodings = self.tokenizer(train_contexts, train_questions, truncation=True, padding=True)
         val_encodings = self.tokenizer(val_contexts, val_questions, truncation=True, padding=True)
         self.add_token_positions(train_encodings, train_answers)
@@ -105,18 +119,22 @@ class HappyBERT(HappyTransformer):
         self.model.to(device)
         self.model.train()
 
+
+        train_dataset = Dataset(train_encodings)
+        val_dataset = Dataset(val_encodings)
         train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
         optim = AdamW(self.model.parameters(), lr=5e-5)
 
         for epoch in range(3):
+            print(epoch)
             for batch in train_loader:
                 optim.zero_grad()
                 input_ids = batch['input_ids'].to(device)
                 attention_mask = batch['attention_mask'].to(device)
                 start_positions = batch['start_positions'].to(device)
                 end_positions = batch['end_positions'].to(device)
-                outputs = model(input_ids, attention_mask=attention_mask, start_positions=start_positions, end_positions=end_positions)
+                outputs = self.model(input_ids, attention_mask=attention_mask, start_positions=start_positions, end_positions=end_positions)
                 loss = outputs[0]
                 loss.backward()
                 optim.step()
